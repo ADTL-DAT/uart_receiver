ee478edda69f21327e3edaa3743a5b6c477cb7fe
kckrithika98 <43531739+kckrithika98@users.noreply.github.com>
2019-01-22 13:35:26 +0530

    Update fifo.v

--- fifo.v
+++ fifo.v
@@ -1,62 +1,52 @@
-module fifo( Clk, 
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/22/2019 12:58:48 PM
+// Design Name: 
+// Module Name: fifo1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
-                   dataIn, 
 
-                   RD, 
+module fifo1( clk,dataIn,RD,WR,dataOut,reset,EMPTY,FULL); 
 
-                   WR, 
+input  clk,RD,WR,reset;
 
-                   EN, 
-
-                   dataOut, 
-
-                   Rst,
-
-                   EMPTY, 
-
-                   FULL 
-
-                   ); 
-
-input  Clk, 
-
-       RD, 
-
-       WR, 
-
-       EN, 
-
-       Rst;
-
-output  EMPTY, 
-
-        FULL;
+output  EMPTY,FULL;
 
 input   [7:0]    dataIn;
 
-output reg [7:0] dataOut; // internal registers 
+output reg [7:0] dataOut; 
 
-reg [3:0]  Count = 0; 
+// internal registers 
 
-reg [7:0] FIFO [0:7]; 
+reg [31:0]  Count = 0; 
 
-reg [2:0]  readCounter = 0, 
+reg [7:0] FIFO [0:1119]; 
 
-           writeCounter = 0; 
+  reg [31:0]  readCounter = 0,writeCounter = 0; 
 
 assign EMPTY = (Count==0)? 1'b1:1'b0; 
 
-assign FULL = (Count==8)? 1'b1:1'b0; 
+assign FULL = (Count==1200)? 1'b1:1'b0; 
 
-always @ (posedge Clk) 
+always @ (posedge clk) 
 
 begin 
 
- if (EN==0); 
-
- else begin 
-
-  if (Rst) begin 
+  if (reset) begin 
 
    readCounter = 0; 
 
@@ -72,7 +62,7 @@ begin
 
   end 
 
-  else if (WR==1'b1 && Count<8) begin
+  else if (WR==1'b1 && Count<1200) begin
    FIFO[writeCounter]  = dataIn; 
 
    writeCounter  = writeCounter+1; 
@@ -81,13 +71,13 @@ begin
 
   else; 
 
- end 
+ 
 
- if (writeCounter==8) 
+ if (writeCounter==1200) 
 
   writeCounter=0; 
 
- else if (readCounter==8) 
+ else if (readCounter==1200) 
 
   readCounter=0; 
 
